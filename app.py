d363a173a51365f281b4d4c2571a3c655cb82fab
georgebalan10 <116486631+georgebalan10@users.noreply.github.com>
2025-05-15 19:24:13 +0300

    Mut bază de date principală în instance/, setez corect SQLAlchemy

--- app.py
+++ app.py
@@ -23,8 +23,10 @@ CORS(app)
 load_dotenv()
 
 # Configurare bază de date
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DATABASE = os.path.join('instance', 'database.db')
 
 # Configurare Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
